063aea3a298b5e03ff957b82a5fdc861475275fe
SithijaDeshan <cst20075@std.uwu.ac.lk>
2026-03-07 23:22:21 +0530

    feat: add DynamoDB table construct and update dependencies
    - Added a new DynamoDBTable construct in infrastructure/dynamoDB.ts to simplify the creation of DynamoDB tables with customizable properties.

---
+++ infrastructure/dynamoDB.ts
@@ -0,0 +1,26 @@
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
+
+interface DynamoDBTableProps {
+  tableName: string;
+  partitionKey: { name: string; type: dynamodb.AttributeType };
+  sortKey?: { name: string; type: dynamodb.AttributeType };
+  billingMode?: dynamodb.BillingMode;
+}
+
+export class DynamoDBTable extends Construct {
+  public readonly table: dynamodb.Table;
+
+  constructor(scope: Construct, id: string, props: DynamoDBTableProps) {
+    super(scope, id);
+
+    this.table = new dynamodb.Table(this, id, {
+      tableName: props.tableName,
+      partitionKey: props.partitionKey,
+      sortKey: props.sortKey,
+      billingMode: props.billingMode || dynamodb.BillingMode.PAY_PER_REQUEST,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+  }
+}
--- infrastructure/stack.ts
+++ infrastructure/stack.ts
@@ -2,6 +2,7 @@ import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { Environment, StackProps } from "aws-cdk-lib";
 import { DDBStreamsLambda } from "./lambda";
+import { DynamoDBTable } from "./dynamoDB";
 
 interface DDBStreamsStackProps extends StackProps {
   envName: string;
@@ -14,6 +15,16 @@ export class DDBStreams extends cdk.Stack {
     super(scope, id, props);
     const environment = `${props.envName}${props.namespace}`;
     const env: Environment = props.env as Environment;
+
+    const coursesTable = new DynamoDBTable(this, "CoursesTable", {
+      tableName: "Courses",
+      partitionKey: {
+        name: "courseId",
+        type: cdk.aws_dynamodb.AttributeType.STRING,
+      },
+      sortKey: { name: "date", type: cdk.aws_dynamodb.AttributeType.STRING },
+    });
+
     new DDBStreamsLambda(this, "ddbstreams-lambda", {
       functionName: `ddbstreams-lambda-${environment}`,
       path: `../src/functions/handler.ts`,
